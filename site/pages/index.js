eb12fb7b647afcd3af273033c2332bc607519031
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-02-10 15:02:24 -0400

    add echarts lib

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,7 +1,59 @@
-export default function Home () {
+import { useEffect, useRef } from 'react'
+import * as echarts from 'echarts'
+
+export const getStaticProps = async () => {
+  const data = {
+    today: [1, 4, 2],
+    week: [1, 4, 2],
+    month: [1, 4, 2]
+  }
+
+  return {
+    props: {
+      data
+    },
+    revalidate: 300
+  }
+}
+
+export default function Home ({ data }) {
+  const chartRef = useRef()
+
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current)
+
+    const option = {
+      title: {
+        text: 'ECharts entry example'
+      },
+      tooltip: {},
+      legend: {
+        data: ['Sales']
+      },
+      xAxis: {
+        data: ['shirt', 'cardign', 'chiffon shirt', 'pants', 'heels', 'socks']
+      },
+      yAxis: {},
+      series: [{
+        name: 'Sales',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+
+    // use configuration item and data specified to show chart
+    myChart.setOption(option)
+  })
+
   return (
     <main>
-      init
+      {
+        Object.keys(data).map(function (key) {
+          return (<li key={key}>{key}</li>)
+        })
+      }
+      <br />
+      <div ref={chartRef} style={{ width: 600, height: 400 }} />
     </main>
   )
 }
